20b27923ee3703e124c1b1194cb2489a5541d039
unknown <hebertsa07@gmail.com>
2017-10-13 17:58:54 -0400

    adding assignment files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'I am a Saiyan?';
+  powerLevel=0;
+  human;
+  saiyan;
+  superSaiyan;
+  superSaiyan2;
+  superSaiyan3;
+  superSaiyan4;
+  type1 = "Human"
+  type2 = "Saiyan"
+  type3 = "SuperSaiyan"
+  type4 = "SuperSaiyan2"
+  type5 = "SuperSaiyan3"
+  type6 = "SuperSaiyan4"
+
+  onSubmit(){
+    this.human = this.powerLevel;
+    this.saiyan = this.powerLevel * 10;
+    this.superSaiyan = this.powerLevel * 90;
+    this.superSaiyan2 = this.powerLevel * 150;
+    this.superSaiyan3 = this.powerLevel * 250;
+    this.superSaiyan4 = this.powerLevel * 500;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TypesComponent } from './types/types.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
